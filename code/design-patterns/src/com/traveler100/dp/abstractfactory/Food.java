b6297b0446cb1bbafe00640b7764831d933c7b83
行百里者 <cpeng8912@gmail.com>
2020-09-16 17:27:13 +0800

    delete old

--- code/design-patterns/src/com/traveler100/dp/abstractfactory/Food.java
+++
@@ -1,9 +0,0 @@
-package com.traveler100.dp.abstractfactory;
-
-/**
- * 食物
- * @author chappell
- */
-public abstract class Food {
-    public abstract void printName();
-}
--- code/design-patterns/src/com/traveler100/dp/proxy/dynamicproxy/Painter.java
+++
@@ -1,9 +0,0 @@
-package com.traveler100.dp.proxy.dynamicproxy;
-
-/**
- * 小画家 接口
- * @author 行百里者
- */
-public interface Painter {
-    void paint();
-}
