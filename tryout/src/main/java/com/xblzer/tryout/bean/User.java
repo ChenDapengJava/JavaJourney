11d6cee9f9bcffc75e2eccdb814a34bac4345548
行百里er <cpeng8912@gmail.com>
2022-08-03 16:01:41 +0800

    增加反射

---
+++ tryout/src/main/java/com/xblzer/tryout/bean/User.java
@@ -0,0 +1,24 @@
+package com.xblzer.tryout.bean;
+
+/**
+ * @author 行百里者
+ * @date 2022-08-03 10:47
+ */
+public class User {
+    private Integer id;
+    private String userName;
+    private String phoneNumber;
+
+    public User() {
+    }
+
+    public User(Integer id, String userName, String phoneNumber) {
+        this.id = id;
+        this.userName = userName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void myDefine() {
+        System.out.println("xxx");
+    }
+}
